5cfe7c2a47f55cb7da64d622488f1c941626cf4a
nayeem-miah <nayeem5113a@gmail.com>
2025-07-26 09:35:57 +0600

    done

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import { app } from "./app";
 import mongoose from "mongoose";
 
 const port = process.env.PORT || 5000;
-// const uri = process.env.MONGO_URI as string
-const uri = "mongodb://localhost:27017/libraryDB"
+const uri = process.env.MONGO_URI as string
+// const uri = "mongodb://localhost:27017/libraryDB"
 
 let server: Server;
 const main = async () => {
